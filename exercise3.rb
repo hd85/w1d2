efde2b8170b973af6a016f2dc263e4ff0ab5c762
Hasnain Dossa <hdossa@Hasnain-Dossas-MacBook-Pro.local>
2015-08-18 16:14:45 -0400

    program to greet user and tell them what year they were born in

--- exercise3.rb
+++ exercise3.rb
@@ -2,7 +2,11 @@ puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}"
 
+puts "How old are you?"
+age = gets.chomp.to_i
+year = 2015 - age
 
+puts "Looks like you were born in #{year}"
 
 
 
