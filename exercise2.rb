37b875fd74088a877da830d5312d5d48daffff46
Hasnain Dossa <hdossa@Hasnain-Dossas-MacBook-Pro.local>
2015-08-18 15:41:20 -0400

    adding code to calculate a tip

---
+++ exercise2.rb
@@ -0,0 +1,58 @@
+# 1 
+
+puts "$#{55 * 0.15}"
+
+
+# 2
+
+a = 55
+b = 0.15
+
+puts "A good tip for a $#{a} meal would be $#{a * b}" # a is the amount of the meal b is the 15% tip
+
+
+# 3
+
+a = 45628
+b = 7839
+
+puts "The value of a times b is #{a * b}" # the value of a times b is 357677892
+
+
+# 4 
+
+puts (true && false) || (false && true) || !(false && false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
